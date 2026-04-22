c6618200c1c578323efbcd7a672d281abaa54480
Jake Soenneker <jake@soenneker.com>
2026-04-21 19:36:12 -0500

    cleanup

--- test/Soenneker.Maf.Pool.Gemini.Tests/MafPoolGeminiExtensionTests.cs
+++ test/Soenneker.Maf.Pool.Gemini.Tests/MafPoolGeminiExtensionTests.cs
@@ -1,16 +1,10 @@
 
 namespace Soenneker.Maf.Pool.Gemini.Tests;
 
-[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
 public sealed class MafPoolGeminiExtensionTests
 {
-    public MafPoolGeminiExtensionTests( output)
-    {
-    }
-
     [Test]
     public void Default()
     {
-
     }
 }
